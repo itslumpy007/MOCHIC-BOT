07e8d5d066668967b63e4285b17d387d46d757ce
lumpy007 <ellisdeoleo0@yahoo.com>
2026-05-24 14:44:45 -0400

    Swap in a new Mochi Bird avatar

--- web/public/mochi/game.js
+++ web/public/mochi/game.js
@@ -9,7 +9,7 @@ const overlayTextEl = document.getElementById('overlayText');
 const primaryButton = document.getElementById('primaryButton');
 const sessionNoteEl = document.getElementById('sessionNote');
 const introSplashEl = document.getElementById('introSplash');
-const ASSET_VERSION = 'transparent1';
+const ASSET_VERSION = 'transparent2';
 
 const params = new URLSearchParams(window.location.search);
 let sessionId = params.get('sid');
@@ -24,7 +24,7 @@ let session = null;
 const logoSprite = new Image();
 logoSprite.src = `./assets/mochi-logo.svg?v=${ASSET_VERSION}`;
 const birdSprite = new Image();
-birdSprite.src = `./assets/avatar.png?v=${ASSET_VERSION}`;
+birdSprite.src = `./assets/avatar-v2.png?v=${ASSET_VERSION}`;
 const canSprite = new Image();
 canSprite.src = `./assets/dr-pepper-can.png?v=${ASSET_VERSION}`;
 
